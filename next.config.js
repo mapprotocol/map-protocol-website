0be64240430d2341f91bcb3eb9722c2f3995ec26
IvanMacaron <ivan.macaron@outlook.com>
2025-04-03 22:51:37 +0800

    add ignore

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 const withMDX = require('@next/mdx')({
-  extension: /\.md$/
+    extension: /\.md$/
 })
 
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config')
+const {i18n} = require('./next-i18next.config')
 
 const nextConfig = {
     i18n,
     images: {
-        domains: ['assets.coingecko.com',"pbs.twimg.com"],
-      },
-      pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+        domains: ['assets.coingecko.com', "pbs.twimg.com"],
+    },
+    pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+    target: 'experimental-serverless-trace',
+    // Optionally, add any other Next.js config below
 }
 
 module.exports = withMDX(nextConfig)
