f27f706588c71c30b321504f05c8d6bc3e89a235
IvanMacaron <ivan.macaron@outlook.com>
2025-09-18 19:48:22 +0800

    add

--- src/pages/brand-assets/top/index.tsx
+++ src/pages/brand-assets/top/index.tsx
@@ -1,41 +1,38 @@
 import Image from 'next/image'
 import styles from './index.module.css'
-import { Button, Box, Popover, Typography } from '@mui/material';
+import {Button, Box, Popover, Typography} from '@mui/material';
 import Link from "next/link";
-import { useTranslation, Trans } from 'next-i18next';
+import {useTranslation, Trans} from 'next-i18next';
 
-import { useEffect, useState } from 'react';
-import { useMapPrice } from '@/hooks/useMapPrice';
+import {useEffect, useState} from 'react';
+import {useMapPrice} from '@/hooks/useMapPrice';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import Router from 'next/router'
-import { handleButtonClick } from '@/utils/dom';
-import { Url } from 'next/dist/shared/lib/router/router';
+import {handleButtonClick} from '@/utils/dom';
+import {Url} from 'next/dist/shared/lib/router/router';
 import CardList from '@/components/ExploreCard';
 import CustomHead from '@/components/CustimHead';
+
 type Props = {
   // Add custom props here
 }
+
 interface CardInfo {
   cardTitle: String,
   cardContent: String,
 }
 
 
-
-export default function HomePageTop(
-
-) {
+export default function HomePageTop() {
   const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const { t } = useTranslation('brand-assets');
+  const {t} = useTranslation('brand-assets');
   const [fade, setFade] = useState(styles.main)
 
 
   useEffect(() => {
     setFade(`${styles.main} ${styles.fade}`)
   }, [])
-
-
 
 
   const logoCard: any[] = [
@@ -141,6 +138,28 @@ export default function HomePageTop(
     },
 
   ]
+  const logoCard2: any[] = [
+    {
+      title: 'Blue White Logo #1',
+      width: "234px",
+      height: "47px"
+    },
+    {
+      title: 'Blue White Logo #2',
+      width: "234px",
+      height: "47px"
+    },
+    {
+      title: 'Blue White Logo #3',
+      width: "234px",
+      height: "80px"
+    },
+    {
+      title: 'Blue White Logo #4',
+      width: "234px",
+      height: "80px"
+    },
+  ]
 
   const badgesCard: any[] = [
     {
@@ -177,18 +196,19 @@ export default function HomePageTop(
       <CustomHead
         title={t('MAP Protocol brand assets') + ' - ' + 'Map Protocol'}
       />
-      <div className={fade} >
+      <div className={fade}>
         <div className={styles.mainLeft}>
           <div className={styles.title}>{t('MAP PROTOCOL LOGO')}</div>
-          <div className={styles.desc}>{t('The MAP Protocol logo is displayed in the official colors and usage patterns. Do not place the logo on top of other objects and leave a reasonable amount of margin around the outside so the logo is clearly visible.')}</div>
+          <div
+            className={styles.desc}>{t('The MAP Protocol logo is displayed in the official colors and usage patterns. Do not place the logo on top of other objects and leave a reasonable amount of margin around the outside so the logo is clearly visible.')}</div>
         </div>
         <div className={styles.topImage}
         >
           <Image
             fill
-            style={{ objectFit: "contain" }}
+            style={{objectFit: "contain"}}
             src="/images/computer.png"
-            alt="community" />
+            alt="community"/>
         </div>
       </div>
       <div>
@@ -201,16 +221,16 @@ export default function HomePageTop(
                 backgroundColor: index % 2 == 0 ? '#fff' : "#1f1f1f"
               }}>
                 <div className={styles.cardTopImage}
-                  style={{
-                    width: item.width,
-                    height: item.height,
-                  }}
+                     style={{
+                       width: item.width,
+                       height: item.height,
+                     }}
                 >
                   <Image
                     fill
-                    style={{ objectFit: "contain" }}
+                    style={{objectFit: "contain"}}
                     src={`/logos/${index + 1}.svg`}
-                    alt={item.title} />
+                    alt={item.title}/>
                 </div>
               </div>
               <div className={styles.cardName}>{item.title}
@@ -234,6 +254,47 @@ export default function HomePageTop(
                 </div>
               </div>
             </div>)}
+          {logoCard2.map((item, index) =>
+            <div
+              key={'logo2' + index}
+              className={styles.card}>
+              <div className={styles.cardTop} style={{
+                backgroundColor: index % 2 == 0 ? '#fff' : "#1f1f1f"
+              }}>
+                <div className={styles.cardTopImage}
+                     style={{
+                       width: item.width,
+                       height: item.height,
+                     }}
+                >
+                  <Image
+                    fill
+                    style={{objectFit: "contain"}}
+                    src={`/logos/${index + 21}.svg`}
+                    alt={item.title}/>
+                </div>
+              </div>
+              <div className={styles.cardName}>{item.title}
+
+                <div className={styles.downloadTabs}>
+                  <a
+                    className={styles.download}
+                    href={`/logos/${index + 21}.svg`} download={`logos.svg`}>
+                    SVG
+                  </a>
+                  <a
+                    className={styles.download}
+                    href={`/logos/${index + 21}.eps`} download={`logos.eps`}>
+                    EPS
+                  </a>
+                  <a
+                    className={styles.download}
+                    href={`/logos/${index + 21}.png`} download={`logos.png`}>
+                    PNG
+                  </a>
+                </div>
+              </div>
+            </div>)}
         </div>
       </div>
 
@@ -251,36 +312,36 @@ export default function HomePageTop(
               backgroundColor: index % 2 == 0 ? '#fff' : "#1f1f1f"
             }}>
               <div className={styles.cardTopImage}
-                style={{
-                  width: item.width,
-                  height: item.height,
-                }}
+                   style={{
+                     width: item.width,
+                     height: item.height,
+                   }}
               >
                 <Image
                   fill
-                  style={{ objectFit: "contain" }}
+                  style={{objectFit: "contain"}}
                   src={`/logos/${index + 17}.svg`}
-                  alt={item.title} />
+                  alt={item.title}/>
               </div>
             </div>
             <div className={styles.cardName}>{item.title}
 
               <div className={styles.downloadTabs}>
-              <a
-                    className={styles.download}
-                    href={`/logos/${index + 17}.svg`} download={`badges.svg`}>
-                    SVG
-                  </a>
-                  <a
-                    className={styles.download}
-                    href={`/logos/${index + 17}.eps`} download={`badges.eps`}>
-                    EPS
-                  </a>
-                  <a
-                    className={styles.download}
-                    href={`/logos/${index + 17}.png`} download={`badges.png`}>
-                    PNG
-                  </a>
+                <a
+                  className={styles.download}
+                  href={`/logos/${index + 17}.svg`} download={`badges.svg`}>
+                  SVG
+                </a>
+                <a
+                  className={styles.download}
+                  href={`/logos/${index + 17}.eps`} download={`badges.eps`}>
+                  EPS
+                </a>
+                <a
+                  className={styles.download}
+                  href={`/logos/${index + 17}.png`} download={`badges.png`}>
+                  PNG
+                </a>
               </div>
             </div>
           </div>)}
