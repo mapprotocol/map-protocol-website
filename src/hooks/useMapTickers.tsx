5551126d432f758c17cc9c5c48e431cf7b38d0b1
IvanMacaron <ivan.macaron@outlook.com>
2026-05-22 17:03:24 +0800

    add

--- src/hooks/useMapTickers.tsx
+++ src/hooks/useMapTickers.tsx
@@ -1,51 +1,53 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import { getData } from "../utils/cache"
-import { absoluteDecimal } from "../utils/number"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { getData } from "../utils/cache";
+import { absoluteDecimal } from "../utils/number";
 
 interface TickersInfo {
-  logo: string
-  name: string
-  pairs: string
-  price: number
-  cost_to_move_up_usd: number
-  cost_to_move_down_usd: number
-  volume: number
+  logo: string;
+  name: string;
+  pairs: string;
+  price: number;
+  cost_to_move_up_usd: number;
+  cost_to_move_down_usd: number;
+  volume: number;
 }
 
-
 export const useMapTickers = () => {
-  const [mapTickers, setMapTickers] = useState<Array<TickersInfo>>([])
+  const [mapTickers, setMapTickers] = useState<Array<TickersInfo>>([]);
   useEffect(() => {
-    ; (async () => {
+    (async () => {
       try {
         const data = await getData<{ tickers: [] }>(
-          "https://api.coingecko.com/api/v3/coins/marcopolo/tickers?include_exchange_logo=true&depth=true"
-        )
-        const {
-          tickers,
-        } = data
-        if (!tickers) throw new Error("Unable to fetch Map tickers from CoinGecko")
-        let tickerArray: Array<TickersInfo> = []
+          "https://api.coingecko.com/api/v3/coins/marcopolo/tickers?include_exchange_logo=true&depth=true",
+        );
+        const { tickers } = data;
+        if (!tickers)
+          throw new Error("Unable to fetch Map tickers from CoinGecko");
+        let tickerArray: Array<TickersInfo> = [];
         tickers.forEach((item: any) => {
-          if(item.market.identifier !=='uniswap_v3')
-          tickerArray.push(
-            {
+          if (
+            item.market.identifier !== "uniswap_v3" &&
+            item.base.startsWith("MAP")
+          )
+            tickerArray.push({
               logo: item.market.logo,
               name: item.market.name,
               pairs: item.base + "/" + item.target,
               price: item.converted_last.usd,
               cost_to_move_up_usd: absoluteDecimal(item.cost_to_move_up_usd, 0),
-              cost_to_move_down_usd: absoluteDecimal(item.cost_to_move_down_usd, 0),
-              volume: absoluteDecimal(item.converted_volume.usd, 0)
-            }
-          )
-        })
-        setMapTickers(tickerArray)
+              cost_to_move_down_usd: absoluteDecimal(
+                item.cost_to_move_down_usd,
+                0,
+              ),
+              volume: absoluteDecimal(item.converted_volume.usd, 0),
+            });
+        });
+        setMapTickers(tickerArray);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    })()
-  }, [])
-  return mapTickers
-}
+    })();
+  }, []);
+  return mapTickers;
+};
--- src/pages/get-mapo/CEXs/index.tsx
+++ src/pages/get-mapo/CEXs/index.tsx
@@ -1,133 +1,229 @@
-import * as React from 'react';
-import styles from './index.module.css'
-import { useTranslation, Trans } from 'next-i18next';
-import Link from 'next/link';
-import { useMapTickers } from '@/hooks/useMapTickers';
-import { toThousands } from '@/utils/number';
-import Image from 'next/image'
-
+import * as React from "react";
+import styles from "./index.module.css";
+import { useTranslation, Trans } from "next-i18next";
+import Link from "next/link";
+import { useMapTickers } from "@/hooks/useMapTickers";
+import { toThousands } from "@/utils/number";
+import Image from "next/image";
 
 const accordionSx = {
-    marginBottom: '10px',
-    backgroundColor: '#F7F7F7',
-    borderRadius: '10px',
-    borderStyle: 'none',
-    boxShadow: 'none',
-    overflow: 'hidden'
-}
+  marginBottom: "10px",
+  backgroundColor: "#F7F7F7",
+  borderRadius: "10px",
+  borderStyle: "none",
+  boxShadow: "none",
+  overflow: "hidden",
+};
 export default function GetMapCEXs() {
-    const [expanded, setExpanded] = React.useState<string | false>(false);
-    const { t } = useTranslation('get-map-1');
-    const tickers = useMapTickers()
-    const steps = [
-        {
-            title: t("Step") + " 1",
-            description: t("Register an account on a CEX's official website or its app. (Check Exchange Ranking by CoinGecko)"),
-            subDescription: t("If a CEX supports one-step sign-up with your social account, you can sign up with your social account directly.")
-        },
-        {
-            title: t("Step") + " 2",
-            description: t("Verify your identity and secure your CEX's account. You are typically required to have a government-issued identification document."),
-            subDescription: t("For asset security, it’s recommended to enable Two-step Verification.")
-        },
-        {
-            title: t("Step") + " 3",
-            description: t("Use fiat to buy USDT, ETH, BTC, or any other tokens supported. You can use the OTC option right in a CEX."),
-            subDescription: t("Additionally, you can use an onramp platform that has your desired fiat-to-crypto option.")
-        },
-        {
-            title: t("Step") + " 4",
-            description: t("Transfer the USDT, BTC, etc. you purchased by fiat to a CEX that supports MAP trading."),
-            subDescription: t("If the CEX you use supports onramp and the MAP-USDT or MAP-BTC trading pair at the same time, you can skip this.")
-        },
-        {
-            title: t("Step") + " 5",
-            description: t("Buy MAP in the spot market via the MAP-USDT or MAP-BTC trading pair."),
-            subDescription: t("In the spot market, select either the “Limit” or “Market” option based on your trading preference.")
-        },
-    ];
-    return (<>
-        <div id={"cexs"} className={styles.exchanges}>
-            <div className={styles.title}>{t('On Centralized Exchanges (CEXs)')} </div>
-            <div className={styles.content}>{t('Centralized exchanges are platforms that are managed by a single entity or company. They act as middlemen between buyers and sellers of cryptocurrencies, similar to traditional stock exchanges.')} </div>
-            <div className={styles.content}>
-                <Trans ns="get-map-1" i18nKey="ERC-20 MAP (MAP Protocol's native token on Ethereum) is available on Kucoin, Bithumb, Coinone and MEXC Global. MRC-20 MAPO (MAP Protocol's native token on MAP Relay Chain thus the MAPO Mainnet) is available on LBank." components={{
-                    link1: <Link target="_blank" href={"https://www.kucoin.com/trade/MAPO-USDT"} className={styles.linkText}></Link>,
-                    link2: <Link target="_blank" href={"https://www.bithumb.com/react/trade/order/MAPO-KRW"} className={styles.linkText}></Link>,
-                    link3: <Link target="_blank" href={"https://coinone.co.kr/exchange/trade/mapo/krw"} className={styles.linkText}></Link>,
-                    link4: <Link target="_blank" href={"https://www.mexc.co/exchange/MAPO_USDT?_from=search"} className={styles.linkText}></Link>,
-                    link5: <Link target="_blank" href={"https://www.lbank.com/trade/mapo_usdt/"} className={styles.linkText}></Link>,
-                    link6: <Link target="_blank" href={"https://www.htx.com/trade/mapo_usdt?type=spot"} className={styles.linkText}></Link>,
-                    link7: <Link target="_blank" href={"https://www.Bitget.com/spot/MAPOUSDT"} className={styles.linkText}></Link>,
-                    link8: <Link target="_blank" href={"https://www.gate.io/trade/MAPO_USDT"} className={styles.linkText}></Link>,
+  const [expanded, setExpanded] = React.useState<string | false>(false);
+  const { t } = useTranslation("get-map-1");
+  const tickers = useMapTickers();
+  const steps = [
+    {
+      title: t("Step") + " 1",
+      description: t(
+        "Register an account on a CEX's official website or its app. (Check Exchange Ranking by CoinGecko)",
+      ),
+      subDescription: t(
+        "If a CEX supports one-step sign-up with your social account, you can sign up with your social account directly.",
+      ),
+    },
+    {
+      title: t("Step") + " 2",
+      description: t(
+        "Verify your identity and secure your CEX's account. You are typically required to have a government-issued identification document.",
+      ),
+      subDescription: t(
+        "For asset security, it’s recommended to enable Two-step Verification.",
+      ),
+    },
+    {
+      title: t("Step") + " 3",
+      description: t(
+        "Use fiat to buy USDT, ETH, BTC, or any other tokens supported. You can use the OTC option right in a CEX.",
+      ),
+      subDescription: t(
+        "Additionally, you can use an onramp platform that has your desired fiat-to-crypto option.",
+      ),
+    },
+    {
+      title: t("Step") + " 4",
+      description: t(
+        "Transfer the USDT, BTC, etc. you purchased by fiat to a CEX that supports MAP trading.",
+      ),
+      subDescription: t(
+        "If the CEX you use supports onramp and the MAP-USDT or MAP-BTC trading pair at the same time, you can skip this.",
+      ),
+    },
+    {
+      title: t("Step") + " 5",
+      description: t(
+        "Buy MAP in the spot market via the MAP-USDT or MAP-BTC trading pair.",
+      ),
+      subDescription: t(
+        "In the spot market, select either the “Limit” or “Market” option based on your trading preference.",
+      ),
+    },
+  ];
+  return (
+    <>
+      <div id={"cexs"} className={styles.exchanges}>
+        <div className={styles.title}>
+          {t("On Centralized Exchanges (CEXs)")}{" "}
+        </div>
+        <div className={styles.content}>
+          {t(
+            "Centralized exchanges are platforms that are managed by a single entity or company. They act as middlemen between buyers and sellers of cryptocurrencies, similar to traditional stock exchanges.",
+          )}{" "}
+        </div>
+        <div className={styles.content}>
+          <Trans
+            ns="get-map-1"
+            i18nKey="ERC-20 MAP (MAP Protocol's native token on Ethereum) is available on Kucoin, Bithumb, Coinone and MEXC Global. MRC-20 MAPO (MAP Protocol's native token on MAP Relay Chain thus the MAPO Mainnet) is available on LBank."
+            components={{
+              link1: (
+                <Link
+                  target="_blank"
+                  href={"https://www.kucoin.com/trade/MAPO-USDT"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link2: (
+                <Link
+                  target="_blank"
+                  href={"https://www.bithumb.com/react/trade/order/MAPO-KRW"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link3: (
+                <Link
+                  target="_blank"
+                  href={"https://coinone.co.kr/exchange/trade/mapo/krw"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link4: (
+                <Link
+                  target="_blank"
+                  href={"https://www.mexc.co/exchange/MAPO_USDT?_from=search"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link5: (
+                <Link
+                  target="_blank"
+                  href={"https://www.lbank.com/trade/mapo_usdt/"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link6: (
+                <Link
+                  target="_blank"
+                  href={"https://www.htx.com/trade/mapo_usdt?type=spot"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link7: (
+                <Link
+                  target="_blank"
+                  href={"https://www.Bitget.com/spot/MAPOUSDT"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+              link8: (
+                <Link
+                  target="_blank"
+                  href={"https://www.gate.io/trade/MAPO_USDT"}
+                  className={styles.linkText}
+                ></Link>
+              ),
+            }}
+          />{" "}
+        </div>
 
-                }} /> </div>
-
-            <div className={styles.form}>
-                <div className={styles.formItem}   >
-                    <div>{t('Exchange')}</div>
-                    <div>{t('Pairs')}</div>
-                    <div>{t('Price')}</div>
-                    <div>{t('+2% Depth')}</div>
-                    <div>{t('-2% Depth')}</div>
-                    <div>{t('24h Volume')}</div>
-                    <div>{t('Updated')}</div>
+        <div className={styles.form}>
+          <div className={styles.formItem}>
+            <div>{t("Exchange")}</div>
+            <div>{t("Pairs")}</div>
+            <div>{t("Price")}</div>
+            <div>{t("+2% Depth")}</div>
+            <div>{t("-2% Depth")}</div>
+            <div>{t("24h Volume")}</div>
+            <div>{t("Updated")}</div>
+          </div>
+          {tickers.map((item, index) => {
+            if (item.name == "PancakeSwap V3 (BSC)") item.pairs = "MAPO/USDC";
+            else if (item.name == "Coinone") item.pairs = "MAPO/KRW";
+            else if (item.name == "Bithumb") item.pairs = "MAPO/KRW";
+            else if (item.name == "Matcha (BSC)") item.pairs = "MAPO/USDC";
+            return (
+              <div className={styles.formItem} key={index}>
+                <div className={styles.dexTitle}>
+                  <img width={28} height={28} src={`${item.logo}`} alt="logo" />
+                  {item.name}
                 </div>
-                {tickers.map((item, index) => {
-                    if (item.name == "PancakeSwap V3 (BSC)")
-                        item.pairs = "MAPO/USDC"
-                    else if (item.name == "Coinone")
-                        item.pairs = "MAPO/KRW"
-                    else if (item.name == "Bithumb")
-                        item.pairs = "MAPO/KRW"
-                    else if (item.name == "Matcha (BSC)")
-                        item.pairs = "MAPO/USDC"
-                    return <div className={styles.formItem} key={index}>
-                        <div className={styles.dexTitle}><img
-                            width={28}
-                            height={28}
-                            src={`${item.logo}`}
-                            alt="logo" />{item.name}</div>
-                        <div>{item.pairs}</div>
-                        <div>{`$${item.price}`}</div>
-                        <div>{`$${toThousands(item.cost_to_move_up_usd)}`}</div>
-                        <div>{`$${toThousands(item.cost_to_move_down_usd)}`}</div>
+                <div>{item.pairs}</div>
+                <div>{`$${item.price}`}</div>
+                <div>{`$${toThousands(item.cost_to_move_up_usd)}`}</div>
+                <div>{`$${toThousands(item.cost_to_move_down_usd)}`}</div>
 
-                        <div>{`$${toThousands(item.volume)}`}</div>
-                        <div>{t('Recently')}</div>
-
-                    </div>
-                })}
-            </div>
-
+                <div>{`$${toThousands(item.volume)}`}</div>
+                <div>{t("Recently")}</div>
+              </div>
+            );
+          })}
         </div>
+      </div>
 
-        <div className={styles.trading}>
-            <div className={styles.tradingTitle}>{t('Trading steps on CEXs')}</div>
-            <div className={styles.tradingContent}>{t("Since the operation procedures vary from different CEXs, we can't provide you a step-by-step guide on how to buy tokens in all the CEXs, but here are some general steps.")}</div>
-            <div className={styles.tradingContent}>{t("Some CEX may not be available in your country or area, you can consult the exchange's customer service or check the announcement from exchanges. As for the tax issue, you may also want to consult the local department for more information.")}</div>
-            <div className={styles.steps} >
-                {steps.map((step, index) => (
-                    <div key={index} className={styles.step} >
-                        <div className={styles.up}>
-                            <div className={styles.round}>{index + 1}</div>
-                            {index !== 4 && <div className={styles.line}></div>}
-                        </div>
-
-                        <div className={styles.stepCard}>
-                            <div className={styles.stepTitle}>{step.title}</div>
-                            <div className={styles.stepDesc}>{index == 0 ? <Trans ns="get-map-1" i18nKey="Register an account on a CEX's official website or its app. (Check Exchange Ranking by CoinGecko)" components={{
-                                link1: <Link target="_blank" href={"https://www.coingecko.com/en/exchanges"} className={styles.linkText}></Link>
-                            }} /> : step.description}</div>
-                            <div className={styles.stepDesc} style={{ marginTop: '16px' }}>{step.subDescription}</div>
-                        </div>
-
-                    </div>
-                ))}
-            </div>
+      <div className={styles.trading}>
+        <div className={styles.tradingTitle}>{t("Trading steps on CEXs")}</div>
+        <div className={styles.tradingContent}>
+          {t(
+            "Since the operation procedures vary from different CEXs, we can't provide you a step-by-step guide on how to buy tokens in all the CEXs, but here are some general steps.",
+          )}
         </div>
+        <div className={styles.tradingContent}>
+          {t(
+            "Some CEX may not be available in your country or area, you can consult the exchange's customer service or check the announcement from exchanges. As for the tax issue, you may also want to consult the local department for more information.",
+          )}
+        </div>
+        <div className={styles.steps}>
+          {steps.map((step, index) => (
+            <div key={index} className={styles.step}>
+              <div className={styles.up}>
+                <div className={styles.round}>{index + 1}</div>
+                {index !== 4 && <div className={styles.line}></div>}
+              </div>
+
+              <div className={styles.stepCard}>
+                <div className={styles.stepTitle}>{step.title}</div>
+                <div className={styles.stepDesc}>
+                  {index == 0 ? (
+                    <Trans
+                      ns="get-map-1"
+                      i18nKey="Register an account on a CEX's official website or its app. (Check Exchange Ranking by CoinGecko)"
+                      components={{
+                        link1: (
+                          <Link
+                            target="_blank"
+                            href={"https://www.coingecko.com/en/exchanges"}
+                            className={styles.linkText}
+                          ></Link>
+                        ),
+                      }}
+                    />
+                  ) : (
+                    step.description
+                  )}
+                </div>
+                <div className={styles.stepDesc} style={{ marginTop: "16px" }}>
+                  {step.subDescription}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-
-    );
+  );
 }
-
